7d8ca92cfa937a41923f25db0447cf89e3999156
aleksandr.markin <aleksandr.markin@iqoption.com>
2018-10-02 21:20:30 +0300

    regex added

--- Rust/crawler/src/main.rs
+++ Rust/crawler/src/main.rs
@@ -1,17 +1,26 @@
 extern crate curl;
-
-use std::io::{stdout, Write};
+extern crate regex;
 
 use curl::easy::Easy;
+use regex::Regex;
 
 fn main() {
     let mut easy = Easy::new();
+    let mut body: &str;
+
     easy.url("https://scanlibs.com/").unwrap();
     easy.write_function(|data| {
-        stdout().write_all(data).unwrap();
+    	body = match std::str::from_utf8(data) {
+	        Ok(v)  => v,
+	        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+	    };
+
         Ok(data.len())
     }).unwrap();
     easy.perform().unwrap();
 
-    println!("{}", easy.response_code().unwrap());
+	let re = Regex::new("<img class=\"alignleft\" src=\"(.+?)\"").unwrap();
+	for cap in re.captures_iter(body) {
+	    println!("{}", &cap[1]);
+	}
 }
