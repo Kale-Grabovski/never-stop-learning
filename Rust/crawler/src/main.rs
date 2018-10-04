e164aed73979bbc015c714d1b5ec2fde8743b0e7
aleksandr.markin <aleksandr.markin@iqoption.com>
2018-10-04 19:10:53 +0300

    regex

--- Rust/crawler/src/main.rs
+++ Rust/crawler/src/main.rs
@@ -6,21 +6,20 @@ use regex::Regex;
 
 fn main() {
     let mut easy = Easy::new();
-    let mut body: &str;
 
     easy.url("https://scanlibs.com/").unwrap();
     easy.write_function(|data| {
-    	body = match std::str::from_utf8(data) {
+    	let body = match std::str::from_utf8(data) {
 	        Ok(v)  => v,
 	        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
 	    };
 
+		let re = Regex::new("(?s)\"alignleft\" src=\"(.+?)\" alt=\"(.+?)\".+?>(.+?)href=\"(.+?)\"").unwrap();
+		for cap in re.captures_iter(body) {
+		    println!("{} {} {} {}", &cap[1], &cap[2], &cap[3], &cap[4]);
+		}
+
         Ok(data.len())
     }).unwrap();
     easy.perform().unwrap();
-
-	let re = Regex::new("<img class=\"alignleft\" src=\"(.+?)\"").unwrap();
-	for cap in re.captures_iter(body) {
-	    println!("{}", &cap[1]);
-	}
 }
