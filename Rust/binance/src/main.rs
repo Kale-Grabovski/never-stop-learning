f38286f1caf881a72a9c0a9eb0ec62da822bc884
aleksandr.markin <aleksandr.markin@iqoption.com>
2018-11-08 17:42:02 +0300

    bin

--- Rust/binance/src/main.rs
+++ Rust/binance/src/main.rs
@@ -5,31 +5,43 @@ extern crate hex;
 
 use sha2::Sha256;
 use hmac::{Hmac, Mac};
+use std::time::{SystemTime, UNIX_EPOCH};
+use reqwest::header;
 
 type HmacSha256 = Hmac<Sha256>;
 
-fn main() {
-    //let uri = "https://api.binance.com/api/v3";
-    // /ticker/price
+fn format_request(query: &str) -> String {
+    let secret = "bM04FLpBLyftHUXajCGDs8i3yqGcUyBLkv1Ptx";
+    let mut mac = HmacSha256::new_varkey(secret.as_bytes()).expect("HMAC error");
+    let start = SystemTime::now();
+    let ts = start.duration_since(UNIX_EPOCH).expect("Time error");
+    let ts = ts.as_secs() * 1000 + ts.subsec_nanos() as u64 / 1_000_000;
 
-    let body = reqwest::get("https://www.rust-lang.org")
+    let input = query.to_owned() + "timestamp=" + &ts.to_string().to_owned();
+    mac.input(input.as_bytes());
+
+    let result = mac.result();
+    let code_bytes = result.code();
+
+    return input + &"&signature=".to_string() + &hex::encode(code_bytes);
+}
+
+fn main() {
+    let uri = "https://api.binance.com/api/v3".to_string();
+    let mut headers = header::HeaderMap::new();
+    headers.insert("X-MBX-APIKEY", header::HeaderValue::from_str("VQlJwQ5dr3BtXHcmE9ZPWNwJVibyuRnlc3").unwrap());
+
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()
+        .unwrap();
+    let url = uri + "/account?" + &format_request("");
+    println!("body = {:?}", url);
+    let body = client.get(&url)
+        .send()
         .unwrap()
         .text()
         .unwrap();
 
-    // Create HMAC-SHA256 instance which implements `Mac` trait
-    let mut mac = HmacSha256::new_varkey(b"NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j")
-        .expect("HMAC can take key of any size");
-    mac.input(b"symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=0.1&recvWindow=5000&timestamp=1499827319559");
-
-    // `result` has type `MacResult` which is a thin wrapper around array of
-    // bytes for providing constant time equality check
-    let result = mac.result();
-    // To get underlying array use `code` method, but be carefull, since
-    // incorrect use of the code value may permit timing attacks which defeat
-    // the security provided by the `MacResult`
-    let code_bytes = result.code();
-    let hash = String::from_utf8_lossy(&code_bytes);
-
-    println!("body = {:?}, {:?}", body, hex::encode(code_bytes));
+    println!("body = {:?}", body);
 }
