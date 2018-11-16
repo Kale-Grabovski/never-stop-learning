54a6b40bd280d2f455bcf41ba7d62688d2754c53
aleksandr.markin <aleksandr.markin@iqoption.com>
2018-11-16 15:37:46 +0300

    binance_api module

---
+++ Rust/binance/src/binance_api/mod.rs
@@ -0,0 +1,123 @@
+extern crate reqwest;
+extern crate hmac;
+extern crate sha2;
+extern crate hex;
+extern crate serde;
+extern crate serde_derive;
+
+use serde_derive::{Serialize, Deserialize};
+use sha2::Sha256;
+use hmac::{Hmac, Mac};
+use std::time::{SystemTime, UNIX_EPOCH};
+use reqwest::header;
+
+pub struct Binance{
+    api_key: String,
+    secret_key: String,
+}
+
+#[derive(Debug)]
+pub struct Ticker{
+    symbol: String,
+    price: f32,
+}
+#[derive(Debug)]
+pub struct Balance{
+    asset: String,
+    free: f32,
+    locked: f32,
+}
+
+#[derive(Serialize, Deserialize)]
+struct BalanceRaw {
+    asset: String,
+    free: String,
+    locked: String,
+}
+#[derive(Serialize, Deserialize)]
+struct BalancesRaw {
+    balances: Vec<BalanceRaw>,
+}
+#[derive(Serialize, Deserialize)]
+struct TickerRaw{
+    symbol: String,
+    price: String,
+}
+
+type HmacSha256 = Hmac<Sha256>;
+
+impl Binance {
+    pub fn new(api_key: String, secret_key: String) -> Binance {
+        Binance{api_key, secret_key}
+    }
+
+    pub fn get_balances(&self) -> Vec<Balance> {
+        let body = self.get_response("/account", "", true);
+        let res: BalancesRaw = serde_json::from_str(&body).unwrap();
+
+        res.balances
+            .iter()
+            .filter_map(|x| {
+                let free = x.free.parse::<f32>().unwrap();
+                let locked = x.locked.parse::<f32>().unwrap();
+
+                if free > 0.0 || locked > 0.0 {
+                    return Some(Balance{asset: x.asset.clone(), free, locked});
+                }
+
+                None
+            })
+            .collect::<Vec<Balance>>()
+    }
+
+    pub fn get_tickers(&self) -> Vec<Ticker> {
+        let body = self.get_response("/ticker/price", "", false);
+        let res: Vec<TickerRaw> = serde_json::from_str(&body).unwrap();
+
+        res
+            .iter()
+            .map(|x| Ticker{
+                symbol: x.symbol.clone(),
+                price: x.price.parse::<f32>().unwrap(),
+            })
+            .collect::<Vec<Ticker>>()
+    }
+
+    fn format_request(&self, path: &str, query: &str) -> String {
+        let mut mac = HmacSha256::new_varkey(self.secret_key.as_bytes()).expect("HMAC error");
+        let start = SystemTime::now();
+        let ts = start.duration_since(UNIX_EPOCH).expect("Time error");
+        let ts = ts.as_secs() * 1000 + ts.subsec_nanos() as u64 / 1_000_000;
+
+        let input = query.to_owned() + "&timestamp=" + &ts.to_string().to_owned();
+
+        mac.input(input.as_bytes());
+
+        let result = mac.result();
+        let code_bytes = result.code();
+
+        path.to_string()
+            + &"?".to_string()
+            + &input
+            + &"&signature=".to_string()
+            + &hex::encode(code_bytes)
+    }
+
+    fn get_response(&self, path: &str, query: &str, sign_request: bool) -> String {
+        let uri = "https://api.binance.com/api/v3".to_string();
+        let mut headers = header::HeaderMap::new();
+        headers.insert("X-MBX-APIKEY", header::HeaderValue::from_str(&self.api_key.to_owned()).unwrap());
+
+        let client = reqwest::Client::builder()
+            .default_headers(headers)
+            .build()
+            .unwrap();
+
+        let url = if sign_request {uri + &self.format_request(path, query)} else {uri + &path};
+        client.get(&url)
+            .send()
+            .unwrap()
+            .text()
+            .unwrap()
+    }
+}
--- Rust/binance/src/main.rs
+++ Rust/binance/src/main.rs
@@ -1,133 +1,24 @@
-extern crate reqwest;
-extern crate hmac;
-extern crate sha2;
-extern crate hex;
-extern crate serde;
-extern crate serde_derive;
-
-use serde_derive::{Serialize, Deserialize};
-use sha2::Sha256;
-use hmac::{Hmac, Mac};
-use std::time::{SystemTime, UNIX_EPOCH};
-use reqwest::header;
 use std::env;
 
-#[derive(Serialize, Deserialize)]
-struct BalanceRaw {
-    asset: String,
-    free: String,
-    locked: String,
-}
-#[derive(Serialize, Deserialize)]
-struct BalancesRaw {
-    balances: Vec<BalanceRaw>,
-}
-#[derive(Debug)]
-struct Balance{
-    asset: String,
-    free: f32,
-    locked: f32,
-}
+mod binance_api;
 
-#[derive(Serialize, Deserialize)]
-struct TickerRaw{
-    symbol: String,
-    price: String,
-}
-#[derive(Debug)]
-struct Ticker{
-    symbol: String,
-    price: f32,
-}
-
-type HmacSha256 = Hmac<Sha256>;
-
-fn format_request(path: &str, query: &str) -> String {
-    let secret = match env::var("BINANCE_SECRET") {
-        Ok(val) => val,
-        Err(_) => panic!("BINANCE_SECRET env is not set"),
-    };
-
-    let mut mac = HmacSha256::new_varkey(secret.as_bytes()).expect("HMAC error");
-    let start = SystemTime::now();
-    let ts = start.duration_since(UNIX_EPOCH).expect("Time error");
-    let ts = ts.as_secs() * 1000 + ts.subsec_nanos() as u64 / 1_000_000;
-
-    let input = query.to_owned() + "&timestamp=" + &ts.to_string().to_owned();
-
-    mac.input(input.as_bytes());
-
-    let result = mac.result();
-    let code_bytes = result.code();
-
-    path.to_string()
-        + &"?".to_string()
-        + &input
-        + &"&signature=".to_string()
-        + &hex::encode(code_bytes)
-}
-
-fn get_response(path: &str, query: &str, sign_request: bool) -> String {
-    let uri = "https://api.binance.com/api/v3".to_string();
-    let mut headers = header::HeaderMap::new();
+fn main() {
     let key = match env::var("BINANCE_KEY") {
         Ok(val) => val,
         Err(_) => panic!("BINANCE_KEY env is not set"),
     };
-    headers.insert("X-MBX-APIKEY", header::HeaderValue::from_str(&key.to_owned()).unwrap());
+    let secret = match env::var("BINANCE_SECRET") {
+        Ok(val) => val,
+        Err(_) => panic!("BINANCE_SECRET env is not set"),
+    };
+    let binance = binance_api::Binance::new(key, secret);
 
-    let client = reqwest::Client::builder()
-        .default_headers(headers)
-        .build()
-        .unwrap();
-
-    let url = if sign_request {uri + &format_request(path, query)} else {uri + &path};
-    client.get(&url)
-        .send()
-        .unwrap()
-        .text()
-        .unwrap()
-}
-
-fn get_balances() -> Vec<Balance> {
-    let body = get_response("/account", "", true);
-    let res: BalancesRaw = serde_json::from_str(&body).unwrap();
-
-    res.balances
-        .iter()
-        .filter_map(|x| {
-            let free = x.free.parse::<f32>().unwrap();
-            let locked = x.locked.parse::<f32>().unwrap();
-
-            if free > 0.0 || locked > 0.0 {
-                return Some(Balance{asset: x.asset.clone(), free, locked});
-            }
-
-            None
-        })
-        .collect::<Vec<Balance>>()
-}
-
-fn get_tickers() -> Vec<Ticker> {
-    let body = get_response("/ticker/price", "", false);
-    let res: Vec<TickerRaw> = serde_json::from_str(&body).unwrap();
-
-    res
-        .iter()
-        .map(|x| Ticker{
-            symbol: x.symbol.clone(),
-            price: x.price.parse::<f32>().unwrap(),
-        })
-        .collect::<Vec<Ticker>>()
-}
-
-fn main() {
-    let balances = get_balances();
+    let balances = binance.get_balances();
     for b in balances {
         println!("{:?}", b);
     }
 
-    let tickers = get_tickers();
+    let tickers = binance.get_tickers();
     for b in tickers.into_iter() {
         println!("{:?}", b);
     }
