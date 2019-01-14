2773a1c5ebaa4e6d4f4496b4071c173b6ee82ed2
li <li>
2019-01-15 00:58:29 +0300

    mark ships as dead

--- Rust/ship/src/main.rs
+++ Rust/ship/src/main.rs
@@ -12,7 +12,19 @@ use sdl2::rect::{Rect};
 use sdl2::render::{Canvas, Texture, TextureCreator};
 
 #[derive(Clone, Copy, PartialEq)]
-enum Cell {Alive, Injured, Dead, Empty, Miss}
+enum CellState {Alive, Injured, Dead, Empty, Miss}
+
+#[derive(Clone)]
+struct Cell {
+	state: CellState,
+	coords: [usize; 4],
+}
+
+impl Cell {
+	fn new() -> Cell {
+        Cell{state: CellState::Empty, coords: [100, 100, 100, 100]}
+	}
+}
 
 #[derive(Copy, Clone, PartialEq)]
 enum State {Me, Enemy, Over}
@@ -36,8 +48,8 @@ impl<'a, 'b> Game<'a, 'b> {
 		cell_size: u32,
 		xy: [i32; 2],
 	) -> Game<'a, 'b> {
-		let mut me = vec![Cell::Empty; 100];
-		let mut enemy = vec![Cell::Empty; 100];
+		let mut me = vec![Cell::new(); 100];
+		let mut enemy = vec![Cell::new(); 100];
 
 		Game::gen_ships(&mut me);
 		Game::gen_ships(&mut enemy);
@@ -92,7 +104,7 @@ impl<'a, 'b> Game<'a, 'b> {
 						t - 11, t - 10, t - 9,
 					];
 					for c in &check_cells {
-                        if *c >= 0 && *c < 100 && cells[*c as usize] != Cell::Empty {
+                        if *c >= 0 && *c < 100 && cells[*c as usize].state != CellState::Empty {
 							is_ok = false;
 							break 'outer;
 						}
@@ -102,8 +114,12 @@ impl<'a, 'b> Game<'a, 'b> {
 				}
 
 				if is_ok {
-					for t in coords {
-                        cells[t as usize] = Cell::Alive;
+					for t in &coords {
+                        cells[*t as usize].state = CellState::Alive;
+                        // save ship coordinates at each ship cell
+						for (k, z) in coords.iter().enumerate() {
+							cells[*t as usize].coords[k] = *z as usize;
+						}
 					}
 					break;
 				}
@@ -145,13 +161,14 @@ impl<'a, 'b> Game<'a, 'b> {
         loop {
             let mut rng = rand::thread_rng();
             let cell = rng.gen_range(0, 100);
-			match self.me[cell] {
-				Cell::Alive => {
+			match self.me[cell].state {
+				CellState::Alive => {
 					::std::thread::sleep(Duration::new(1, 0));
-					self.me[cell] = Cell::Injured;
+					self.me[cell].state = CellState::Injured;
+					self.check_dead_ship(false, cell);
 				},
-				Cell::Empty => {
-					self.me[cell] = Cell::Miss;
+				CellState::Empty => {
+					self.me[cell].state = CellState::Miss;
 					self.set_state(State::Me);
 					break;
                 }
@@ -159,6 +176,49 @@ impl<'a, 'b> Game<'a, 'b> {
 			}
 		}
 	}
+
+	fn check_dead_ship(&mut self, is_enemy: bool, cell: usize) {
+		let cells = match is_enemy {
+			true => &mut self.enemy,
+			false => &mut self.me,
+		};
+
+		let mut is_dead = true;
+		for i in &cells[cell].coords {
+			if i < &100 && cells[*i].state == CellState::Alive {
+				is_dead = false;
+			}
+		}
+
+		if !is_dead {
+			return;
+		}
+        for i in &cells[cell].coords.clone() {
+            if i == &100 {
+                continue;
+            }
+
+            let u = *i as i16;
+            let around_cell: [i16; 8] = [
+                u + 1, u - 1,
+                u + 11, u + 10, u + 9,
+                u - 11, u - 10, u - 9,
+            ];
+
+            for k in &around_cell {
+                if k < &0 || k >= &100 {
+                    continue;
+                }
+
+				match cells[*k as usize].state {
+					CellState::Injured | CellState::Dead => {},
+					_ => cells[*k as usize].state = CellState::Miss,
+				}
+            }
+
+			cells[*i as usize].state = CellState::Dead;
+        }
+    }
 
 	pub fn shot(&mut self, x: i32, y: i32) {
 		let cell_x = (x - (self.xy[0] + (self.field_size as i32) + self.fields_space_px)) / self.cell_size as i32;
@@ -168,11 +228,14 @@ impl<'a, 'b> Game<'a, 'b> {
 		}
 
 		let cell = (cell_x + cell_y * 10) as usize;
-        match self.enemy[cell] {
-            Cell::Alive => self.enemy[cell] = Cell::Injured,
-			Cell::Injured | Cell::Dead => {},
+        match self.enemy[cell].state {
+            CellState::Alive => {
+				self.enemy[cell].state = CellState::Injured;
+				self.check_dead_ship(true, cell);
+			},
+			CellState::Injured | CellState::Dead => {},
 			_ => {
-				self.enemy[cell] = Cell::Miss;
+				self.enemy[cell].state = CellState::Miss;
 				self.set_state(State::Enemy);
 			},
 		}
@@ -202,8 +265,8 @@ impl<'a, 'b> Game<'a, 'b> {
 				let x = (i as u32 % 10 * cell_size) as i32 + 1;
 				let y = (i as u32 / 10 * cell_size) as i32 + 1;
 
-				match cell {
-					Cell::Miss => {
+				match cell.state {
+					CellState::Miss => {
 						tc.set_draw_color(Color::RGB(255, 255, 255));
 						tc.draw_rect(Rect::new(
 							x + (cell_size / 2) as i32,
@@ -212,15 +275,15 @@ impl<'a, 'b> Game<'a, 'b> {
 							2,
 						));
 					},
-					Cell::Alive if !is_enemy => {
+					CellState::Alive if !is_enemy => {
 						tc.set_draw_color(Color::RGB(225, 225, 225));
 						tc.fill_rect(Rect::new(x, y, cell_size - 1, cell_size - 1));
 					},
-					Cell::Injured => {
+					CellState::Injured => {
 						tc.set_draw_color(Color::RGB(0, 155, 155));
 						tc.fill_rect(Rect::new(x, y, cell_size - 1, cell_size - 1));
 					},
-					Cell::Dead => {
+					CellState::Dead => {
 						tc.set_draw_color(Color::RGB(215, 215, 0));
 						tc.fill_rect(Rect::new(x, y, cell_size - 1, cell_size - 1));
 					},
